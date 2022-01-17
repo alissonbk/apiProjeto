2a252137be68ef39feddd1ad9c06722e17521f02
Alisson <alissonbk2010@hotmail.com>
2022-01-17 14:01:45 -0300

    Migration change

--- apiProjetoNegocio/src/main/resources/db/migration/V2__Inser_User_Admin.sql
+++ apiProjetoNegocio/src/main/resources/db/migration/V2__Inser_User_Admin.sql
@@ -1,4 +1,4 @@
 INSERT INTO endereco(id, cep, numero, rua, bairro, cidade, estado)
-VALUES(1, '96990000', 15, 'Av. Lauro Billig de Castilhos', 'Centro', 'Estrela Velha', 'RS');
+VALUES(1, '96990000', 10, 'Av. Teste', 'Centro', 'Estrela Velha', 'RS');
 INSERT INTO usuario(email, senha, ativo, nome, cpf, endereco_id, tipo)
-VALUES('admin@email.com', '$2a$12$mrI/C.5ZyMUB9VmETKX/r.IGT2jyiJK41gc0snfHnKQ1dr9T78VmW', true, 'Alisson', '02928180095', 1, 'ADMIN');
+VALUES('admin@email.com', '$2a$12$mrI/C.5ZyMUB9VmETKX/r.IGT2jyiJK41gc0snfHnKQ1dr9T78VmW', true, 'Alisson', '93804787126', 1, 'ADMIN');
